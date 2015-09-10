34c13e46528990d0feb0782eb430c8369a1c2f82
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2015-09-10 17:25:12 -0400

    Second pass

--- eager-qzzr.js
+++ eager-qzzr.js
@@ -1,11 +1,15 @@
 (function(){
-  window.EagerQzzr = {
-    init: function(container, id) {
-      if (!id || !container || !container.parentNode) {
-        return;
-      }
+  var options, container;
 
-      container.innerHTML = '<iframe id="quizWidget-' + id + '" width="100%" height="700px" frameborder="0" border="none" src="https://app.qzzr.com/quiz/' + id + '/widget"></iframe>';
+  options = INSTALL_OPTIONS;
+
+  document.addEventListener('DOMContentLoaded', function(){
+    container = Eager.createElement(options.container);
+
+    if (!options.id || !container || !container.parentNode) {
+      return;
     }
-  };
-});
+
+    container.innerHTML = '<iframe id="quizWidget-' + id + '" width="100%" height="700px" frameborder="0" border="none" src="https://app.qzzr.com/quiz/' + id + '/widget"></iframe>';
+  });
+})();
