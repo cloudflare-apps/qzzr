6850b729c1da2fe9b07e09662da88d19abffc0e9
Zack Bloom <zackbloom@gmail.com>
2016-03-28 13:31:28 -0400

    Add support for multiple quizzes

--- eager-qzzr.js
+++ eager-qzzr.js
@@ -1,15 +1,37 @@
 (function(){
-  var options, container;
+  if (!document.addEventListener)
+    return;
 
-  options = INSTALL_OPTIONS;
+  var options = INSTALL_OPTIONS;
 
-  document.addEventListener('DOMContentLoaded', function(){
-    container = Eager.createElement(options.container);
-
-    if (!options.id || !container || !container.parentNode) {
+  var rendered = false;
+  render = function(){
+    if (rendered)
       return;
+    rendered = true;
+
+    if (!options.quizzes && options.id){
+      options.quizzes = [
+        {
+          id: options.id,
+          container: options.container
+        }
+      ];
     }
 
-    container.innerHTML = '<iframe id="quizWidget-' + options.id + '" width="100%" height="700px" frameborder="0" border="none" src="https://app.qzzr.com/quiz/' + options.id + '/widget"></iframe>';
-  });
+    for (var i=0; i < options.quizzes.length; i++){
+      var quiz = options.quizzes[i];
+
+      if (!quiz.id)
+        continue;
+
+      var container = Eager.createElement(quiz.container);
+      if (!container || !container.parentNode)
+        continue;
+
+      container.innerHTML = '<iframe id="quizWidget-' + quiz.id + '" width="100%" height="700px" frameborder="0" border="none" src="https://app.qzzr.com/quiz/' + quiz.id + '/widget"></iframe>';
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', render);
 })();
